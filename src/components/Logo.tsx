b837b9cfbd4baeb5619b5b6fad5727d035d460a5
dv0c <tasosmeidanis12@gmail.com>
2024-07-16 14:26:27 +0300

    modified:   src/components/Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { ExternalLink } from "lucide-react"
 const Logo = () => {
     return <h1 className="select-none flex relative flex-col group w-full">
         <span className="absolute -top-2 flex justify-end text-[11px] leading-[.50] w-full text-end group-hover:translate-y-0 translate-y-3 opacity-0 group-hover:opacity-100 delay-75 transition-all"><ExternalLink size={15} /></span>
-        <span className="text-3xl font-[800]">MeinDesk.</span>
+        <span className="max-[390px]:text-2xl text-3xl font-[800]">MeinDesk.</span>
         <span className="text-[11px] leading-[.50]">Productivity</span>
     </h1>
 }
